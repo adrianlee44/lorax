d9da954323ba8c23d74740d4c43120a4f9ec5854
Adrian Lee <adrianlee44@gmail.com>
2014-12-10 09:37:49 +0000

    test(lorax): Add linkToIssue tests

---
+++ test/linkToIssue_test.js
@@ -0,0 +1,30 @@
+var lorax = require('../index');
+var linkToIssue = lorax.linkToIssue;
+
+exports.linkToIssueTest = {
+  basicTest: function(test) {
+    var output = linkToIssue('123');
+    
+    test.equal(output, '[#123](https://github.com/adrianlee44/lorax/issues/123)');
+    test.done();
+  },
+  noUrlTest: function(test) {
+    lorax.config.set('url', '');
+    
+    var output = linkToIssue('123');
+    test.equal(output, '#123');
+    test.done();
+  },
+  noIssueTemplateTest: function(test) {
+    lorax.config.set('issue', '');
+    
+    var output = linkToIssue('123');
+    test.equal(output, '#123');
+    test.done();
+  },
+  noIssueTest: function(test) {
+    var output = linkToIssue();
+    test.equal(output, '');
+    test.done();
+  }
+};
