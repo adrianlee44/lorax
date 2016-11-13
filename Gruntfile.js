fe73f818ac405bf07e6d3c1b40c61cefdeee32e2
Adrian Lee <adrianlee44@gmail.com>
2016-11-13 00:14:45 -0800

    chore(Grunt): Update ava task for ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     ava: {
-      target: ["test/**/*_test.js"],
       options: {
-        nyc: true,
-        require: [
-          'babel-register'
-        ]
-      }
+        require: ['babel-register']
+      },
+      ci: {
+        options: {
+          nyc: true
+        },
+        files: {
+          src: ['test/**/*_test.js']
+        }
+      },
+      test: ['test/**/*_test.js']
     },
     eslint: {
       options: {
@@ -15,32 +20,32 @@ module.exports = function(grunt) {
       },
       src: {
         files: {
-          src: ["src/**/*.js"]
+          src: ['src/**/*.js']
         }
       },
       test: {
         files: {
-          src: ["test/**/*_test.js"]
+          src: ['test/**/*_test.js']
         }
       },
       gruntfile: {
         files: {
-          src: ["Gruntfile.js"]
+          src: ['Gruntfile.js']
         }
       }
     },
     watch: {
       gruntfile: {
-        files: "<%= eslint.gruntfile.files.src %>",
-        tasks: ["eslint:gruntfile"]
+        files: '<%= eslint.gruntfile.files.src %>',
+        tasks: ['eslint:gruntfile']
       },
       src: {
-        files: "<%= eslint.src.files.src %>",
-        tasks: ["eslint:src", "ava"]
+        files: '<%= eslint.src.files.src %>',
+        tasks: ['eslint:src', 'ava']
       },
       test: {
-        files: "<%= eslint.test.files.src %>",
-        tasks: ["eslint:test", "ava"]
+        files: '<%= eslint.test.files.src %>',
+        tasks: ['eslint:test', 'ava']
       }
     },
     babel: {
@@ -57,5 +62,6 @@ module.exports = function(grunt) {
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask("default", ["eslint", "ava"]);
+  grunt.registerTask('default', ['eslint', 'ava']);
+  grunt.registerTask('ci', ['eslint', 'ava:ci']);
 };
