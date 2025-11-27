06dae0f3d3d291d70ca3b472122770ee194ec10b
Adrian Lee <208855+adrianlee44@users.noreply.github.com>
2025-11-26 22:40:53 -0800

    test(lorax): Fix test script

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,5 @@ import {createDefaultEsmPreset} from 'ts-jest';
 const presetConfig = createDefaultEsmPreset();
 export default {
   ...presetConfig,
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
 } satisfies Config;
